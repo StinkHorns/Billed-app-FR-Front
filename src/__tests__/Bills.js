95ab04ddd74557df7db50ceb941be2ffa18f5083
StinkHorns <marcodelolmo@hotmail.com>
2024-09-28 10:51:34 +0200

    Tests, not working 100%

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -18,6 +18,7 @@ describe("Given I am connected as an employee", () => {
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
       }))
+
       const root = document.createElement("div")
       root.setAttribute("id", "root")
       document.body.append(root)
@@ -26,8 +27,14 @@ describe("Given I am connected as an employee", () => {
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
+      
+      
+//Marco
+
+      expect(windowIcon.classList.contains("active-icon"))
 
     })
+
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
@@ -35,5 +42,133 @@ describe("Given I am connected as an employee", () => {
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
+
+
   })
 })
+
+
+// MARCO 14
+describe('Given I am connected as an Employee', () => {
+ describe('When on the Bills page and I click on the EYE Button ', () => {
+  test("Then, Then opens the modal",  () => {
+     
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+  
+
+      const handleClickIconEye = jest.fn(bills.handleClickIconEye)
+      const eye = screen.getByTestId('icon-eye')
+      eye.addEventListener('click', handleClickIconEye)
+      userEvent.click(eye)
+
+      expect(handleClickIconEye).toHaveBeenCalled()
+
+      const modale = screen.getByTestId('modaleFile')
+      expect(modale).toBeTruthy()
+       
+
+    
+  }) })
+})
+
+
+//  MARCO 20
+describe('Given I am connected as an Employee', () => {
+describe('When on Bills page and I click on New Bill Button ', () => {
+  test("Then, Then it opens New Bills page",  () => {
+  
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+    window.localStorage.setItem('user', JSON.stringify({
+      type: 'Employee'      
+    }))
+
+    const handleClickNewBill = jest.fn(bills.handleClickNewBill)
+    const eye = screen.getByTestId('btn-new-bill')
+    eye.addEventListener('click', handleClickNewBill)
+    userEvent.click(eye)
+
+    expect(handleClickNewBill).toHaveBeenCalled()
+
+    const newBill = screen.getByTestId('btn-new-bill')
+    expect('btn-new-bill').toBeTruthy()
+  
+
+      }) })
+})
+
+
+
+
+//Marco 36
+describe('Given I am connected as an Employee', () => {
+describe('When on the NEW Bills page and I input a date ', () => {
+  test("Then, it only accepts the right date format",  () => {
+
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+    window.localStorage.setItem('user', JSON.stringify({
+      type: 'Employee'
+    }))
+
+        document.body.innerHTML = BillsUI({ error: 'some error message' })
+        expect(screen.getAllByText('Erreur')).toBeTruthy()
+   
+
+
+  
+      })})
+})
+
+
+// Test ERROR 404 and 500
+
+
+
+describe("When an error occurs on API", () => {
+  beforeEach(() => {
+    jest.spyOn(mockStore, "bills")
+    Object.defineProperty(
+        window,
+        'localStorage',
+        { value: localStorageMock }
+    )
+    window.localStorage.setItem('user', JSON.stringify({
+      type: 'Employee',
+      email: "a@a"
+    }))
+    const root = document.createElement("div")
+    root.setAttribute("id", "root")
+    document.body.appendChild(root)
+    router()
+  })
+  test("fetches bills from an API and fails with 404 message error", async () => {
+
+    mockStore.bills.mockImplementationOnce(() => {
+      return {
+        list : () =>  {
+          return Promise.reject(new Error("Erreur 404"))
+        }
+      }})
+    window.onNavigate(ROUTES_PATH.Bills)
+    await new Promise(process.nextTick);
+    const message = await screen.getByText(/Erreur 404/)
+    expect(message).toBeTruthy()
+  })
+
+  test("fetches messages from an API and fails with 500 message error", async () => {
+
+    mockStore.bills.mockImplementationOnce(() => {
+      return {
+        list : () =>  {
+          return Promise.reject(new Error("Erreur 500"))
+        }
+      }})
+
+    window.onNavigate(ROUTES_PATH.Bills)
+    await new Promise(process.nextTick);
+    const message = await screen.getByText(/Erreur 500/)
+    expect(message).toBeTruthy()
+  })
+}) 
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -6,13 +6,86 @@ import { screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 
-
+/// 6
 describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+  describe("When I am on NewBill Page and I click on Envoyer", () => {
+    test("Then it submits the New Bill info", () => {
       const html = NewBillUI()
-      document.body.innerHTML = html
+      document.body.innerHTML = NewBillUI(bills[0])
+
+
       //to-do write assertion
+
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      const store = null
+      const bilss = new Bills({
+        document, onNavigate, store, bills, localStorage: window.localStorage
+      })
+      const submitButton = screen.getByTestId("form-new-bill")
+      const handleSubmit = jest.fn((e) => bills.handleSubmit(e, bills[0]))
+      submitButton.addEventListener("click", handleSubmit)
+      fireEvent.click(handleButton)
+      expect(handleSubmit).toHaveBeenCalled()
+
     })
   })
 })
+
+
+/// 
+
+/// Choose file
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page and Choose file", () => {
+    test("Then it opens window to select file", () => {
+
+      const html = NewBillUI()
+      document.body.innerHTML = NewBillUI(bills[0])
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+
+      const store = null
+      const bilss = new Bills({
+        document, onNavigate, store, bills, localStorage: window.localStorage
+      })
+      
+      const chooseFile = screen.getByTestId("file")
+      const sendFile = jest.fn((e) => bills.sendFile(e, bills[0]))
+      chooseFile.addEventListener("click", sendFile)
+      fireEvent.click(sendFile)
+      expect(sendFile).toHaveBeenCalled()
+
+    })
+  })
+})
+
+/// Input Date
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page and click on Date", () => {
+    test("Then it will let me inpute date ", () => {
+
+      
+      
+    })
+  })
+})
+
+/// Input Date
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page and click on Type de dépense", () => {
+    test("Then an option has to be chosen ", () => {
+
+      
+      
+    })
+  })
+})
+
